510bbb5f70bcf90004ecd06e2b52104e126cd15b
stanislawwus <vus.pn@ucu@edu.ua>
2024-04-20 19:39:49 +0300

    update docstring

--- new_create_table.py
+++ new_create_table.py
@@ -1,8 +1,12 @@
 '''Example'''
 EXAMPLE = (20, 15)
 
-def create_table(n, m, lst = None):
-    '''fff'''
+def create_table(n, m, lst=None):
+    """
+    Function for creating table for a game
+    >>> create_table(4,6) 
+    [[1, 1, 1, 1, 1, 1], [1, 2, 3, 4, 5, 6], [1, 3, 6, 10, 15, 21], [1, 4, 10, 20, 35, 56]]
+    """
     if lst is None:
         lst = [1]*m
     table = [list(lst)]
